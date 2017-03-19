8448ed19f59b0d3fc6650e60fa56eee65e8c5d0d
Gaurav Bangar <gbangar212@gmail.com>
2017-03-19 23:16:58 +0530

    auto redirect to dashboard

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,16 @@
 class HomepageController < ApplicationController
+  before_action :authorize #uncomment this once the site has to go live
   def home
+
+  end
+
+  protected
+  def authorize
+    interviewer = Interviewer.find_by(id: session[:user_id])
+    if interviewer.blank?
+    else
+      session[:user_id] = interviewer.id
+      redirect_to interviewer_dashboard_url
+    end
   end
 end
